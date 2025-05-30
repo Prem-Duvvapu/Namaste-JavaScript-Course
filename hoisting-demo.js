54ee91e2ca5c40a79ffd50dac62d9804c671920b
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-05-30 18:51:16 +0000

    hoisting and functions working

---
+++ hoisting-demo.js
@@ -0,0 +1,18 @@
+getName();
+console.log(x);
+
+console.log(getName);
+
+var x=7;
+
+// function getName() {
+//     console.log("Luffy");
+// }
+
+var getName = () => {
+    console.log("Zoro");
+}
+
+var getName2 = function () {
+    
+}
