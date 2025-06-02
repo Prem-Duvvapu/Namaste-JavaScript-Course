49a3f0aef6d9648aac7222f58528e513add5611a
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-06-02 15:01:37 +0000

    block scope

---
+++ block-scope-demo.js
@@ -0,0 +1,15 @@
+var a=100;
+
+{
+    var a=10;
+    let b=20;
+    const c=30;
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+console.log(a);
+// console.log(b); //gives ReferenceError: b is not defined
+// console.log(c); //gives ReferenceError: b is not defined
